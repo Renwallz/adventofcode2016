52e765697e6f02bbca5763eba4c85433de22fda7
Renwallz <renwick90@gmail.com>
2016-12-09 21:32:10 +1100

    Part 2 done!
    Music makes coding fun!

--- day03.py
+++ day03.py
@@ -2,11 +2,21 @@
 
 possibleCount = 0
 with open('day03.in') as f:
+    
+    # now we need to be tricky; grab 3 lines at a time
+    
+    currentTriangles = []
     for line in f:
         line = line.rstrip()
         values = [int(x) for x in line.split()]
-        values.sort()
-        if values[0] + values[1] > values[2]:
-            possibleCount += 1
+        currentTriangles.append(values)
+        
+        if len(currentTriangles) == 3:
+            for i in range(3):
+                values = [x[i] for x in currentTriangles]
+                values.sort()
+                if values[0] + values[1] > values[2]:
+                    possibleCount += 1
+            currentTriangles = []
             
 print("There are {} Triangles that are possible".format(possibleCount))
