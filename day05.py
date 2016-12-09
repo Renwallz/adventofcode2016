193f5cb84df4ade8017596eb2c4c2a0a6a721d2b
Renwallz <renwick90@gmail.com>
2016-12-10 10:42:37 +1100

    Day 5 part 2 animated
    Animation is fun

--- day05.py
+++ day05.py
@@ -3,20 +3,39 @@
 # Lets verify some bitcoins!
 
 import hashlib
-
+import random
 
 DOORID = b'ugkcyxxp'
+PASSWORDLENGTH = 8
 
-key = []
+key = list('u' * PASSWORDLENGTH)
 i = 0
+
+HASHCODES = '123456789abcdef'
+
+def animate(key):
+    line = []
+    for c in key:
+        if c == 'u':
+            line.append( random.choice( HASHCODES ) ) 
+        else:
+            line.append( c )
+    print('\r Password: {}'.format(''.join(line) ), end='' )
 
 while True:
     hash = hashlib.md5( DOORID + str(i).encode('ascii') ).hexdigest()
     if hash[:5] == '00000':
-        key.append(hash[5])
-        print ("Found stage {}".format( len(key) ) )
-        if len(key) == 8:
-            break
+        pos = hash[5]
+        value = hash[6]
+        if pos.isnumeric():
+            pos = int(pos)
+            if pos < PASSWORDLENGTH and key[pos] == 'u':
+                key[pos] = value
+                if 'u' not in key:
+                    print('\r '+' ' * 40 ) # clear the screen
+                    break
     i += 1
+    if i % 40000 == 0: # chosen by science to be the most exciting speed on  my cpu
+        animate(key)
             
 print ("The password is {}".format(''.join(key) ) )
